741e828f0f3c34a8610ea364eb410f85cc5c50c6
vivekdesai22 <vivekdesai_22@yahoo.com>
2014-03-02 12:48:13 -0500

    Initial Commit

---
+++ TweetTrends/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TweetTrends
+//
+//  Created by Vivek Desai on 3/2/14.
+//  Copyright (c) 2014 Vivek Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
